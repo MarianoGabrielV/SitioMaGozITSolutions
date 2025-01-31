321741b2127985888aa28bc83d0ad601636be704
MarianoVergniaud <mariano.unla@gmail.com>
2025-01-31 13:10:14 -0300

    nuevoFormatoDelSitio

--- js/script.js
+++ js/script.js
@@ -1,24 +1,6 @@
-// Smooth scrolling para los enlaces del navbar
-document.querySelectorAll('.nav-link').forEach(link => {
-    link.addEventListener('click', function(e) {
-        e.preventDefault();
-        const target = document.querySelector(this.getAttribute('href'));
-        target.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
-    });
-});
+// Inicializar animaciones AOS
+AOS.init();
 
-// Animaciones simples al cargar las secciones
-window.addEventListener('scroll', () => {
-    const elements = document.querySelectorAll('.animate-on-scroll');
-    elements.forEach(el => {
-        const position = el.getBoundingClientRect().top;
-        if (position < window.innerHeight - 100) {
-            el.classList.add('animate');
-        }
-    });
-});
+
 
 
